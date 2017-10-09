72999774ad709f543ffc3a47ab9e278949cf248c
1412268 <1412268@student.hcmus.edu.vn>
2017-10-09 20:12:52 +0700

    Add About page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,6 @@ module ApplicationHelper
         "active" if current_page?(path)
     end
     def google_map(lat, long)
-        "https://maps.googleapis.com/maps/api/staticmap?center=#{lat},#{long}&zoom=18&size=900x300&maptype=roadmap&format=png&visual_refresh=true&markers=size:mid%7Ccolor:red%7Clabel:1%7C#{lat},#{long}&key= AIzaSyCBp54KmSB-wZAJMB4_Y9yWVJyGYVYFzt4"
+        "https://maps.googleapis.com/maps/api/staticmap?center=#{lat},#{long}&zoom=15&size=900x300&maptype=roadmap&format=png&visual_refresh=true&markers=size:mid%7Ccolor:red%7Clabel:1%7C#{lat},#{long}&key= AIzaSyCBp54KmSB-wZAJMB4_Y9yWVJyGYVYFzt4"
     end
 end
