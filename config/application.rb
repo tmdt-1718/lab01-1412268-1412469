f4b1c56eb88b7b2aea4544f3bcee9c0448d3b01f
1412268 <1412268@student.hcmus.edu.vn>
2017-10-09 21:08:40 +0700

    switched to import syntax

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,7 @@ module Lab0114122681412469
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.assets.paths << Rails.root.join('vendor', 'assets')
-    config.assets.paths << "#{Rails.root}/vendor/assets"
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
