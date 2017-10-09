3f8b3fc306b90d243cd01870b8b8dec488278a1f
1412268 <1412268@student.hcmus.edu.vn>
2017-10-09 16:48:19 +0700

    add login, signup page with bcrypt, logout and user status

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get 'sessions/new'
+
+  get '/blog' => 'blog#index'
+  get '/albums' => 'albums#index'
+  get '/about' => 'about#index'
+  get '/home' => 'home#index'
   root "home#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 end
